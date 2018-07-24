99cd3b4bda49c29c3d1e7691f5d005c278ce9c10
liuxiang <1355070604@qq.com>
2018-07-24 10:12:01 +0800

    s随记

--- server2.js
+++ server2.js
@@ -1,15 +1,17 @@
 var http = require('http');
 var url = require('url');
 
-function start () {
+function start (route) {
   function onRequest (req, res) {
     var pathname = url.parse(req.url).pathname;
     console.log('Request for' + pathname + 'received.');
+    route(pathname);
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.write('Hello world');
     res.end();
   }
 
   http.createServer(onRequest).listen(8888);
+  console.log('Server has started.')
 }
 exports.start = start;
